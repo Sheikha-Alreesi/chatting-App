4cdf6dbbac8d7516f4f5a6c392468d089f3b68d5
alree <201811117@gcet.edu.om>
2023-05-26 20:43:00 +0400

    Finished project

---
+++ app/src/main/java/com/example/scaledrone/chat/LoginActivity.java
@@ -0,0 +1,41 @@
+package com.example.scaledrone.chat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Button button;
+    TextView button2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        button = (Button) findViewById(R.id.btnLogin);
+        button2 = (TextView) findViewById(R.id.lnkRegister);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, Registration.class);
+                startActivity(intent);
+
+            }
+        });
+
+    }
+}
